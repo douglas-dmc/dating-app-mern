5bc32d4b7f8d2944d9909761541514c79dc9668d
Douglas Marques <douglasmarques37@gmail.com>
2023-09-08 10:52:11 -0300

    feat: criando o componente DatingCards.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './components/Header/Header'
+import DatingCards from "./components/DatingCards/DatingCards"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <div>
         <Header />
+        <DatingCards />
       </div>
     </>
   )
